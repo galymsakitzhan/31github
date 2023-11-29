edf97bc4189a9d6fbe4d4816280299e544c3ffb6
Galym <galymsakitzhan@gmail.com>
2023-11-29 23:01:25 +0600

    commit bab2

--- basket.js
+++ basket.js
@@ -1,4 +1,16 @@
 const openShopping = document.querySelector('.openShopping');
 const closeShopping = document.querySelector('.closeShopping');
 const list = document.querySelector('.list');
-const listCard = document.querySelector('.listCard')
+const listCard = document.querySelector('.listCard');
+const total = document.querySelector('.total');
+const body = document.querySelector("body");
+const quantity = document.querySelector(".quantity");
+
+openShopping.addEventListener("click", () => {
+  body.classList.add("active");
+})
+closeShopping.addEventListener("click", () => {
+  body.classList.remove("active");
+})
+
+
