bf40653d91abc39afb4b0e5ddc879f1d94f8ab71
Galym <galymsakitzhan@gmail.com>
2023-11-29 23:05:35 +0600

    commit bab3

--- basket.js
+++ basket.js
@@ -13,4 +13,42 @@ closeShopping.addEventListener("click", () => {
   body.classList.remove("active");
 })
 
-
+let prodcuts = [
+  {
+    id:1,
+    name:"Product 1",
+    images:"1.PNG",
+    price: 2000
+  },
+  {
+    id:1,
+    name:"Product 2",
+    images:"2.PNG",
+    price: 2000
+  },
+  {
+    id:1,
+    name:"Product 3",
+    images:"3.PNG",
+    price: 2000
+  },
+  {
+    id:1,
+    name:"Product 4",
+    images:"4.PNG",
+    price: 2000
+  },
+  {
+    id:1,
+    name:"Product 5",
+    images:"5.PNG",
+    price: 2000
+  },
+  {
+    id:1,
+    name:"Product 6",
+    images:"6.PNG",
+    price: 2000
+  },
+  
+]
