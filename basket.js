6fce7c08a38ae0a8bff0bc7d85a7c6e794dab4e8
Galym <galymsakitzhan@gmail.com>
2023-11-29 22:58:24 +0600

    commit baby

---
+++ basket.js
@@ -0,0 +1,4 @@
+const openShopping = document.querySelector('.openShopping');
+const closeShopping = document.querySelector('.closeShopping');
+const list = document.querySelector('.list');
+const listCard = document.querySelector('.listCard')
