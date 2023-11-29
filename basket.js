0407d6f489c63b24f2d05a500d227e9d43fbc9ca
Galym <galymsakitzhan@gmail.com>
2023-11-29 23:08:03 +0600

    commit bab7

--- basket.js
+++ basket.js
@@ -24,31 +24,42 @@ let prodcuts = [
     id:1,
     name:"Product 2",
     images:"2.PNG",
-    price: 2000
+    price: 2300
   },
   {
     id:1,
     name:"Product 3",
     images:"3.PNG",
-    price: 2000
+    price: 1700
   },
   {
     id:1,
     name:"Product 4",
     images:"4.PNG",
-    price: 2000
+    price: 1000
   },
   {
     id:1,
     name:"Product 5",
     images:"5.PNG",
-    price: 2000
+    price: 3000
   },
   {
     id:1,
     name:"Product 6",
     images:"6.PNG",
-    price: 2000
-  },
-  
+    price: 2500
+  }, 
 ]
+
+let listCards = [];
+
+const initApp = () => {
+  prodcuts.forEach(value,key) => {
+    let newDiv = document.createElement("div")
+    newDiv.classList.add("item");
+    newDiv.innerHTML = `
+    
+    `
+  }
+}
